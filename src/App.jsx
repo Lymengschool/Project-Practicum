69dded5c68463ba3a85fc3370d1b4c6cb260e1e0
Lymengschool <134981562+Lymengschool@users.noreply.github.com>
2024-01-13 20:24:49 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      
+    }
+  ])
+}
+
+export default App
