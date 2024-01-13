a5eb2170cc8c61a4c8b36a5ca2e053b995aa76ce
Lymengschool <tek.lymeng.2821@rupp.edu.kh>
2024-01-13 20:42:31 +0700

    write here

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import './App.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Nav from "./components/nav.jsx";
 
 function App() {
   const router = createBrowserRouter([
     {
-      
+      path: "/",
+      element: <Nav/>
     }
   ])
+
+  return (
+    <RouterProvider router={router} />
+  );
 }
 
 export default App
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-function Nav(){
+function nav(){
     return(
         <nav>
-            d
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About Us</a></li>
+                <li><a href="/contact">Contact Us</a></li>
+            </ul>
         </nav>
     )
 }
+
+export default nav
