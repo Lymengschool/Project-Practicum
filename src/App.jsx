c41645e7553cee04162be43c0dd8c8f9d372ed71
Lymengschool <tek.lymeng.2821@rupp.edu.kh>
2024-01-14 20:16:23 +0700

    fdddf

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Nav from "./components/nav.jsx";
+import Footer from "./components/footer.jsx";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Nav/>
+    },
+    {
+      path: "/footer",
+      element: <Footer/>
     }
   ])
 
---
+++ src/components/footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram, faTelegram } from '@fortawesome/free-brands-svg-icons'; 
+import style from './../../public/css/footer.module.css';
+
+function Footer() {
+    return (
+        <footer className={style.container}>
+                         <Link to="#" className={style.icon}>
+                            <FontAwesomeIcon icon={faFacebook} />
+                        </Link>
+                        <Link to="#" className={style.icon}>
+                            <FontAwesomeIcon icon={faInstagram} />
+                        </Link>
+                        <Link to="#" className={style.icon}>
+                            <FontAwesomeIcon icon={faTelegram} />
+                        </Link>            
+        </footer>
+    );
+}
+
+export default Footer;
