90b1717046cfadca6b0998dc5f0840ea929f91d4
TMS <taimengseu7777@gmail.com>
2024-01-14 14:13:05 +0700

    new code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Nav from "./components/nav.jsx";
+import Footer from "./components/footer.jsx";
 
 function Appp() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Nav/>
+    },
+    {
+      path: "/f",
+      element: <Footer/>
     }
   ])
 
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram, faTelegram } from '@fortawesome/free-brands-svg-icons'; 
+import style from './../../public/css/footer.module.css';
 
-function footer(){
-    return(
-        <nav>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About Us</a></li>
-                <li><a href="/contact">Contact Us</a></li>
-            </ul>
-        </nav>
-    )
+function Footer() {
+    return (
+        <footer className={style.container}>
+                    <Link to="#" className={style.icon}>
+                        <FontAwesomeIcon icon={faFacebook} />
+                    </Link>
+                    <Link to="#" className={style.icon}>
+                        <FontAwesomeIcon icon={faInstagram} />
+                    </Link>
+                    <Link to="#" className={style.icon}>
+                        <FontAwesomeIcon icon={faTelegram} />
+                    </Link>
+        </footer>
+    );
 }
 
-export default footer;
+export default Footer;
