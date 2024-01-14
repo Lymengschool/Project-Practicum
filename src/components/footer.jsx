a579efedb7246f06f456d268c7af24beefafa0af
TMS <taimengseu7777@gmail.com>
2024-01-14 13:05:59 +0700

    footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function footer(){
+    return(
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About Us</a></li>
+                <li><a href="/contact">Contact Us</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default footer;
