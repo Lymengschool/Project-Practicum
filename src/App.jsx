c82884110f5649d57cba29214df474e3b7c70c6a
mengseuTao99 <taimengseu7777@gmail.com>
2024-01-15 22:18:01 +0700

    d

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,7 @@ function App() {
       path: "/",
       element: <Home/>
     },
-<<<<<<< HEAD
-    {
-<<<<<<< HEAD
-      path: "/f",
-=======
-      path: "/footer",
->>>>>>> d425ef01d540f848b767bb80ea0a77abf4759089
-      element: <Footer/>
-    }
-=======
->>>>>>> 51feb6f40e9635974ae03e44365f86d3896a2b3c
+
   ])
 
   return (
