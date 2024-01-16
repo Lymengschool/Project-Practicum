d12deed1ad13ca322364c1f4ccb3bbae3dc248d0
mengseuTao99 <taimengseu7777@gmail.com>
2024-01-16 17:31:54 +0700

    change

---
+++ src/components/firebase.jsx
@@ -0,0 +1,32 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider } from "firebase/auth";
+import { getDatabase, ref, set } from "firebase/database";
+import { signInWithRedirect, signInWithPopup, FacebookAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBl6rvFMcaoh0P87U3lhmgSQkSgjTwyOzg",
+  authDomain: "jai-typi.firebaseapp.com",
+  projectId: "jai-typi",
+  storageBucket: "jai-typi.appspot.com",
+  messagingSenderId: "978339587144",
+  appId: "1:978339587144:web:899c827c22d6f0b62fe47d",
+  measurementId: "G-ST3BWXETBF"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const database = getDatabase();
+
+export {
+  app,
+  auth,
+  GoogleAuthProvider,
+  database,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  set,
+  signInWithRedirect,
+  signInWithPopup,
+  ref,
+  FacebookAuthProvider
+};
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,7 +1,63 @@
 import React from "react";
 import style from './../../public/css/form.module.css';
+import {app, auth, database, createUserWithEmailAndPassword, signInWithEmailAndPassword,set, ref} from './firebase'
+import { useState } from "react";
 
-function Form({ name, confirmpassword, action}) {
+function Form({ name, confirmpassword, action }) {
+  const [Name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [Confirmpassword, setConfirmPassword] = useState('');
+
+  const handleButtonClick = () => {
+
+    // logic for login and register
+    if (action === "login") {
+      // validate input fields
+      if (!validateEmail(email) || !validatePassword(password)) {
+        alert('Email or Password is Outta Line!!');
+        return;
+      }
+
+      signInWithEmailAndPassword(auth, email, password)
+        .then(() => {
+          alert('Logged in success!!');
+        })
+        .catch(error => {
+          const error_message = error.message;
+          alert(error_message);
+        });
+    } else if (action === "register") {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          const database_ref = ref(database);
+
+          const user_data = {
+            user_name: Name,
+            email: email,
+          };
+
+          set(ref(database, 'users/' + user.uid), user_data);
+          alert('User Created!!');
+        })
+        .catch(error => {
+          const error_code = error.code;
+          const error_message = error.message;
+          alert(error_message);
+        });
+    }
+  };
+
+  const validateEmail = (email) => {
+    const expression = /^[^@]+@\w+(\.\w+)+\w$/;
+    return expression.test(email);
+  };
+
+  const validatePassword = (password) => {
+    return password.length >= 6;
+  };
+ 
   return (
     <body className={style.body}>
        <form className={style.contianer}>
@@ -9,10 +65,10 @@ function Form({ name, confirmpassword, action}) {
           <input
             className={style.input}
             type="text"
-            id="name"
+            id="Name"
             placeholder="ឈ្មោះ"
-            // value={email}
-            // onChange={(e) => setEmail(e.target.value)}
+            value={Name}
+            onChange={(e) => setName(e.target.value)}
           />
         )}
           <input
@@ -20,32 +76,32 @@ function Form({ name, confirmpassword, action}) {
             type="email"
             id="email"
             placeholder="អ៊ីមែល"
-            // value={email}
-            // onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
            className={style.input}
             type="password"
             id="password"
             placeholder="ពាក្យសម្ងាត់"
-            // value={password}
-            // onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         
          {confirmpassword && (
           <input
           className={style.input}
-            type="confirmpassword"
-            id="confirmpassword"
+            type="password"
+            id="Confirmpassword"
             placeholder="ផ្ទៀងផ្ទាត់ពាក្យសម្ងាត់"
-            // value={password}
-            // onChange={(e) => setPassword(e.target.value)}
+            value={Confirmpassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
           />
         )}
         <input
         className={style.button}
         type="button"
-        // onClick={handleButtonClick}
+        onClick={handleButtonClick}
         value={action === "login" ? "ចូល" : "ចុះឈ្មោះ"}
       />
       </form>
