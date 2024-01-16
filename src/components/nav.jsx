05374797a466c7e283a29ee75939ead4d0015b39
Lymengschool <tek.lymeng.2821@rupp.edu.kh>
2024-01-17 00:19:09 +0700

    sound

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,9 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faKeyboard, faGear, faCircleUser } from "@fortawesome/free-solid-svg-icons";
+import {Howl} from 'howler';
 import nav from "../../public/css/nav.module.css";
 
+const sound = new Howl({
+        src: ['../../public/audio/snail.mp3']
+});
+
 function Nav() {
+    
+    const playSound = () => {
+        sound.stop();
+        sound.play();
+    }
+    
     return (
         <nav>
             <div className={nav.left}>
@@ -25,7 +36,7 @@ function Nav() {
                     <FontAwesomeIcon icon={faGear} />
                 </Link>
                 <Link to='/login' className={nav.icon}>
-                    <FontAwesomeIcon icon={faCircleUser} />
+                    <FontAwesomeIcon icon={faCircleUser} onClick={playSound}/>
                 </Link>
             </div>
         </nav>
