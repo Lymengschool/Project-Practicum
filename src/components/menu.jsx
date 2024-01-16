a6f12d69ecead4a81a95cf264f3d24e7b8a09926
Lymengschool <tek.lymeng.2821@rupp.edu.kh>
2024-01-16 17:38:40 +0700

    mm

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -5,45 +5,36 @@ import { faClock, faQuoteLeft, faBrain, faWrench } from "@fortawesome/free-solid
 
 function Menu() {
     return (
-        <div className={menu.menublock}>
-            <div className={menu.innerleft}>
-                <div>
-                    <FontAwesomeIcon icon={faClock} />
-                    ពេល
+        <div className={menu.container}>
+            <div className={menu.menublock}>
+                <div className={menu.innerleft}>
+                    <button>
+                        <FontAwesomeIcon icon={faClock} />
+                        ពេល
+                    </button>
+                    <button>
+                        <FontAwesomeIcon icon={faQuoteLeft} />
+                        ពាក្យស្លោក
+                    </button>
+                    <button>
+                        <FontAwesomeIcon icon={faBrain} />
+                        សេរីឆ្នៃប្រឌិត
+                    </button>
+                    <button>
+                        <FontAwesomeIcon icon={faWrench} />
+                        ផ្ទាល់ខ្លួន
+                    </button>
                 </div>
-                <div>
-                    <FontAwesomeIcon icon={faQuoteLeft} />
-                    ពាក្យស្លោក
-                </div>
-                <div>
-                    <FontAwesomeIcon icon={faBrain} />
-                    សេរីឆ្នៃប្រឌិត
-                </div>
-                <div>
-                    <FontAwesomeIcon icon={faWrench} />
-                    ផ្ទាល់ខ្លួន
-                </div>
-            </div>
-
-            <div className={menu.innerright}>
-                <div>
-                    <FontAwesomeIcon icon={faClock} />
-                    ពេល
-                </div>
-                <div>
-                    <FontAwesomeIcon icon={faQuoteLeft} />
-                    ពាក្យស្លោក
-                </div>
-                <div>
-                    <FontAwesomeIcon icon={faBrain} />
-                    សេរីឆ្នៃប្រឌិត
-                </div>
-                <div>
-                    <FontAwesomeIcon icon={faWrench} />
-                    ផ្ទាល់ខ្លួន
+                <div className={menu.time}>
+                    <button>30 វិ.</button>
+                    <button>60 វិ.</button>
+                    <button>90 វិ.</button>
+                    <button>120 វិ.</button>
+                    <form action="">
+                        <input type="num" />
+                    </form>
                 </div>
             </div>
-
         </div>
     );
 }
