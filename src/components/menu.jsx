1818631290937f5b5e5a105637db6ec6d5b4915b
Lymengschool <tek.lymeng.2821@rupp.edu.kh>
2024-01-16 14:14:43 +0700

    menu

---
+++ src/components/menu.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import menu from "./../../public/css/menu.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock, faQuoteLeft, faBrain, faWrench } from "@fortawesome/free-solid-svg-icons";
+
+function Menu() {
+    return (
+        <div className={menu.menublock}>
+            <div className={menu.innerleft}>
+                <div>
+                    <FontAwesomeIcon icon={faClock} />
+                    ពេល
+                </div>
+                <div>
+                    <FontAwesomeIcon icon={faQuoteLeft} />
+                    ពាក្យស្លោក
+                </div>
+                <div>
+                    <FontAwesomeIcon icon={faBrain} />
+                    សេរីឆ្នៃប្រឌិត
+                </div>
+                <div>
+                    <FontAwesomeIcon icon={faWrench} />
+                    ផ្ទាល់ខ្លួន
+                </div>
+            </div>
+
+            <div className={menu.innerright}>
+                <div>
+                    <FontAwesomeIcon icon={faClock} />
+                    ពេល
+                </div>
+                <div>
+                    <FontAwesomeIcon icon={faQuoteLeft} />
+                    ពាក្យស្លោក
+                </div>
+                <div>
+                    <FontAwesomeIcon icon={faBrain} />
+                    សេរីឆ្នៃប្រឌិត
+                </div>
+                <div>
+                    <FontAwesomeIcon icon={faWrench} />
+                    ផ្ទាល់ខ្លួន
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Menu;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Nav from "../components/nav.jsx";
 import Footer from "../components/footer.jsx";
-
+import Menu from "../components/menu.jsx";
 
 function Home() {
     return (
         <div>
             <Nav/>
+            <Menu/>
             <Footer/>
         </div>
     );
