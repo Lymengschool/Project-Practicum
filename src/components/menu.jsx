e282e6441830f14c2941e13871b2bc67b79e23ed
Lymengschool <tek.lymeng.2821@rupp.edu.kh>
2024-01-16 21:32:30 +0700

    menu

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,39 +1,66 @@
-import React from "react";
+import React, {useState} from "react";
 import menu from "./../../public/css/menu.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClock, faQuoteLeft, faBrain, faWrench } from "@fortawesome/free-solid-svg-icons";
+import { faClock, faQuoteLeft, faBrain, faWrench, faScrewdriverWrench } from "@fortawesome/free-solid-svg-icons";
+
+
 
 function Menu() {
+    
+    const [activeButton, setActiveButton] = useState(1);
+
     return (
         <div className={menu.container}>
             <div className={menu.menublock}>
                 <div className={menu.innerleft}>
-                    <button>
-                        <FontAwesomeIcon icon={faClock} />
-                        ពេល
+                    <button onClick={() => setActiveButton(1)}>
+                        <span>
+                            <FontAwesomeIcon className={menu.icon} icon={faClock} />
+                            ពេល
+                        </span>
                     </button>
-                    <button>
-                        <FontAwesomeIcon icon={faQuoteLeft} />
-                        ពាក្យស្លោក
+                    <button onClick={() => setActiveButton(2)}>
+                        <span>
+                            <FontAwesomeIcon className={menu.icon} icon={faQuoteLeft} />
+                            ពាក្យស្លោក
+                        </span>
                     </button>
-                    <button>
-                        <FontAwesomeIcon icon={faBrain} />
-                        សេរីឆ្នៃប្រឌិត
+                    <button onClick={() => setActiveButton(3)}>
+                        <span>
+                            <FontAwesomeIcon className={menu.icon} icon={faBrain} />
+                            សេរីឆ្នៃប្រឌិត
+                        </span>
                     </button>
-                    <button>
-                        <FontAwesomeIcon icon={faWrench} />
-                        ផ្ទាល់ខ្លួន
+                    <button onClick={() => setActiveButton(4)}>
+                        <span>
+                            <FontAwesomeIcon className={menu.icon} icon={faWrench} />
+                            ផ្ទាល់ខ្លួន
+                        </span>
                     </button>
                 </div>
-                <div className={menu.time}>
-                    <button>30 វិ.</button>
-                    <button>60 វិ.</button>
-                    <button>90 វិ.</button>
-                    <button>120 វិ.</button>
-                    <form action="">
-                        <input type="num" />
-                    </form>
+
+                <div className={`${menu.innerright} ${activeButton === 1 ? menu.active : menu.none}`}>
+                    <button><span>30 វិ.</span></button>
+                    <button><span>60 វិ.</span></button>
+                    <button><span>90 វិ.</span></button>
+                    <button><span>120 វិ.</span></button>
+                    <button>
+                        <span>
+                            <FontAwesomeIcon className={menu.icon} icon={faScrewdriverWrench} />
+                        </span>
+                    </button>
                 </div>
+
+                <div className={`${menu.innerright} ${activeButton === 2 ? menu.active : menu.none}`}>
+                    <button><span>ខ្លី</span></button>
+                    <button><span>មធ្យម</span></button>
+                    <button><span>វែង</span></button>
+                </div>
+
+                <div className={`${menu.innerright} ${activeButton === 4 ? menu.active : menu.none}`}>
+                    <button><span>ផ្លាស់ប្តូ</span></button>
+                </div>
+
             </div>
         </div>
     );
