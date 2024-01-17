5b60908e957ec5bed4fdc688172c694c3e3d1510
Lymengschool <tek.lymeng.2821@rupp.edu.kh>
2024-01-17 22:25:18 +0700

    repsonsize nav

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,28 +2,27 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faKeyboard, faGear, faCircleUser } from "@fortawesome/free-solid-svg-icons";
-import {Howl} from 'howler';
+import { Howl } from "howler";
 import nav from "../../public/css/nav.module.css";
 
 const sound = new Howl({
-        src: ['../../public/audio/snail.mp3']
+    src: ["../../public/audio/snail.mp3"],
 });
 
 function Nav() {
-    
     const playSound = () => {
         sound.stop();
         sound.play();
-    }
-    
+    };
+
     return (
         <nav>
             <div className={nav.container}>
                 <div className={nav.left}>
                     <Link to='/'>
-                        <img src='../../img/logo.svg' alt='logo' />
+                        <img className={nav.logo} src='../../img/logo.svg' alt='logo' />
                     </Link>
-                    <div>
+                    <div className={nav.logoname}>
                         <p>Ultimate typing Simulator</p>
                         <p>Jai Type</p>
                     </div>
@@ -36,7 +35,7 @@ function Nav() {
                         <FontAwesomeIcon icon={faGear} />
                     </Link>
                     <Link to='/login' className={nav.icon}>
-                        <FontAwesomeIcon icon={faCircleUser} onClick={playSound}/>
+                        <FontAwesomeIcon icon={faCircleUser} onClick={playSound} />
                     </Link>
                 </div>
             </div>
--- src/components/toggle.jsx
+++ src/components/toggle.jsx
@@ -4,12 +4,12 @@ import style from './../../public/css/toggle.module.css'
 function toggle() { // Change the function name to use PascalCase
 
     const  toggleButton = () => {
-        const toggleButton = document.querySelector('.toggle-button');
+        const toggleButton = document.querySelector('.toggle_button');
         toggleButton.classList.toggle('active');
-      }
+    }
 
     return (
-        <div className={style.toggle-button} onclick="toggleButton()">
+        <div className={style.toggle_button} onclick={toggleButton}>
             <div>Toggle</div>
             <div>Button</div>
         </div>
@@ -17,10 +17,3 @@ function toggle() { // Change the function name to use PascalCase
 }
 
 export default toggle;
-
-
-
-
-
-
-
