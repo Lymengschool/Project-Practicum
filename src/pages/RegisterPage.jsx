0c8014b4ccdfa5ba9ea2747ea26c11f61450d91f
mengseuTao99 <taimengseu7777@gmail.com>
2024-01-17 19:44:19 +0700

    r

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -5,6 +5,7 @@ import Nav from '../components/nav';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, toast } from 'react-toastify';
 import Footer from './../components/footer';
+import { Link } from 'react-router-dom';
 
 
 function Register() {
@@ -15,9 +16,13 @@ function Register() {
                 <div className={style.container}>
                     <h1 className={style.tittle}>ចូលគណនេយ្យ</h1>
                     <Form name={true} confirmpassword={true} action={"register"}/>
-                    <ToastContainer />      
+                    <ToastContainer />
+                    <div className={style.textContainer}>
+                        <p className={style.text}>មានគណនេយ្យ ?
+                        <Link to="/login" className={style.create}>&nbsp;ចូល</Link></p>
+                    </div>      
                 </div>
-                <Footer />
+                
             </div>
         </div>
        
