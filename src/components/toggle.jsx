cdfe4596eac30dcf21ac72af5e4b295d57373bc5
Lymengschool <tek.lymeng.2821@rupp.edu.kh>
2024-01-18 02:43:11 +0700

    remade

--- src/components/toggle.jsx
+++ src/components/toggle.jsx
@@ -9,25 +9,10 @@ function Toggle() {
         setIsChecked(!isChecked);
     }
 
-
-    const updateSlider = () => {
-        var checkbox = document.querySelector('.toggle-btn input');
-        var slider = document.querySelector('.toggle-btn .toggle-slider');
-    
-        if (checkbox.checked) {
-            slider.style.transform = 'translateX(30px)';
-            slider.style.backgroundColor = '#ffff00';
-        } else {
-            slider.style.transform = 'translateX(0)';
-            slider.style.backgroundColor = '#fff';
-        }
-    
-    }
-
     return (
-        <label className={style.togglebutton}>
+        <label className={style.toggleBtn}>
             <input type="checkbox" checked={isChecked} onChange={handleToggle}/>
-            <span className={`${style.slider} ${isChecked ? style.active : ''}`}/>
+            <span className={`${style.toggleSlider} ${isChecked ? style.active : ''}`}/>
         </label>
     );
 }
